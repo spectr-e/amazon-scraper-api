971b4516d58c02d58b5536d3c56509acc476c43c
spectr-e <devs.josia@gmail.com>
2022-12-22 18:44:07 +0300

    Added Scraper API, baseurl, get prod details func

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 use the dev command to run application while building it
 express: we use it to create a backend application
 request - promise: we use it to make API requests
+ScraperAPI : We use it to scrape useful data from the amazon website. It allows to scrape html off any site and turn it into a JSON formatted piece of content.
--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const express = require("express");
 const request = require("request-promise");
-const app = express(); // init our application
-const PORT = process.env.PORT || 5000; // assigning a static port from where we deploy our application
 
-app.use(express.json()); // allows our application to parse JSON input
+//creating a node js express backend javascript server with root route
+// init our application
+const app = express(); 
+// assigning a static port from where we deploy our application
+const PORT = process.env.PORT || 5000; 
+//adding apikey from Scraper API that turns any HTML from any website into useful JSON
+const apiKey = "a644d6298b755630a074bd871e5b35cd";
+// the autoparse true enables our application parse JSON input
+const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+
+ 
+app.use(express.json()); 
+// creating a route for our express application (root)
 app.get("/", (req, res) => {
-  res.send("Welcome to Amazon Scraper API"); // returns this statement when user visits the root url
-}); // creating a route for our express application (root)
-app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`)) // makes the server listen on the specified port
+    // returns this statement when user visits the root url
+  res.send("Welcome to Amazon Scraper API"); 
+}); 
+
+// GET Product Details
+// essentially creating a new route, the colon makes the prodID dynamic
+app.get("/products/:productId", async (req, res) => {
+  const { productId } = req.params;
+  try {
+    const response = await request(
+      `${baseUrl}&url=https://www.amazon.com/dp/${productId}`
+    );
+    res.json(response);
+  } catch (error) {
+    res.json(error);
+  }
+});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); // makes the server listen on the specified port
