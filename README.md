f52128bf272d8cf0bca031514471f2d05972bcb9
spectr-e <devs.josia@gmail.com>
2022-12-22 19:38:56 +0300

    Added the search query, gitignore, update README

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-use the dev command to run application while building it
+we are going to create 4 endpoints
+1.
+2.
+3.
+4.
+then host it on heroku
+use the dev command to run application while building it8
 express: we use it to create a backend application
 request - promise: we use it to make API requests
 ScraperAPI : We use it to scrape useful data from the amazon website. It allows to scrape html off any site and turn it into a JSON formatted piece of content.
+we do not make the scraperapikey publicly visible hence we will require every user to have their own apikey
--- index.js
+++ index.js
@@ -3,53 +3,73 @@ const request = require("request-promise");
 
 //creating a node js express backend javascript server with root route
 // init our application
-const app = express(); 
+const app = express();
 // assigning a static port from where we deploy our application
-const PORT = process.env.PORT || 5000; 
-//adding apikey from Scraper API that turns any HTML from any website into useful JSON
-const apiKey = "a644d6298b755630a074bd871e5b35cd";
-// the autoparse true enables our application parse JSON input
-const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+const PORT = process.env.PORT || 5000;
+//dynamic (from user) Scraper API apikey that turns any HTML from any website into useful JSON
+//call it from every endpoint, add query param to every get request that allows users to enter their apikeys
+const genScrapUrl = (apiKey) => {
+  `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+};
 
- 
-app.use(express.json()); 
+app.use(express.json());
 // creating a route for our express application (root)
 app.get("/", (req, res) => {
-    // returns this statement when user visits the root url
-  res.send("Welcome to Amazon Scraper API"); 
-}); 
+  // returns this statement when user visits the root url
+  res.send("Welcome to Amazon Scraper API");
+});
 
-// 1st EndPoint - GET Product Details 
+// 1st EndPoint - GET Product Details
 // essentially creating a new route, the colon makes the prodID dynamic
 app.get("/products/:productId", async (req, res) => {
   const { productId } = req.params;
+  const { api_key } = req.query;
   try {
     const response = await request(
-      `${baseUrl}&url=https://www.amazon.com/dp/${productId}`
+      `${genScrapUrl(api_key)}&url=https://www.amazon.com/dp/${productId}`
     );
     res.json(JSON.parse(response)); // making the response readable with parse func
   } catch (error) {
     res.json(error);
   }
 });
-// 2nd EndPoint - GET Product Reviews 
+// 2nd EndPoint - GET Product Reviews
 app.get("/products/:productId/reviews", async (req, res) => {
   const { productId } = req.params;
+  const { api_key } = req.query;
   try {
     const response = await request(
-      `${baseUrl}&url=https://www.amazon.com/product-reviews/${productId}`
+      `${genScrapUrl(
+        api_key
+      )}&url=https://www.amazon.com/product-reviews/${productId}`
     );
     res.json(JSON.parse(response)); // making the response readable with parse func
   } catch (error) {
     res.json(error);
   }
 });
-// 3rd EndPoint - GET Product Offers 
+// 3rd EndPoint - GET Product Offers
 app.get("/products/:productId/offers", async (req, res) => {
   const { productId } = req.params;
+  const { api_key } = req.query;
   try {
     const response = await request(
-      `${baseUrl}&url=https://www.amazon.com/gp/offer-listing/${productId}`
+      `${genScrapUrl(
+        api_key
+      )}&url=https://www.amazon.com/gp/offer-listing/${productId}`
+    );
+    res.json(JSON.parse(response)); // making the response readable with parse func
+  } catch (error) {
+    res.json(error);
+  }
+});
+// 4th EndPoint - GET Search Results
+app.get("/search/:searchQuery", async (req, res) => {
+  const { searchQuery } = req.params;
+  const { api_key } = req.query;
+  try {
+    const response = await request(
+      `${genScrapUrl(api_key)}&url=https://www.amazon.com/s?k=${searchQuery}`
     );
     res.json(JSON.parse(response)); // making the response readable with parse func
   } catch (error) {
